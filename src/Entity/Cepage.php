269b36c7528f614f71c6e83dda4637b40980f0f6
unknown <nicolassam33@gmail.com>
2023-06-20 21:19:22 +0200

    create entity and fixture Cepages

---
+++ src/Entity/Cepage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CepageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CepageRepository::class)]
+class Cepage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nameCepage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameCepage(): ?string
+    {
+        return $this->nameCepage;
+    }
+
+    public function setNameCepage(string $nameCepage): static
+    {
+        $this->nameCepage = $nameCepage;
+
+        return $this;
+    }
+}
