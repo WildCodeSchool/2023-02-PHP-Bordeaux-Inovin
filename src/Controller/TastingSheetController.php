0eb142b0562bd0ef3f2bc71cde0960aec9e44c81
unknown <nicolassam33@gmail.com>
2023-06-23 14:50:31 +0200

    update TastingController width code Workshop

--- src/Controller/TastingSheetController.php
+++ src/Controller/TastingSheetController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Entity\Smell;
+use App\Entity\Workshop;
 use App\Entity\TastingSheet;
 use App\Form\CommentType;
 use App\Form\TastingSheetType;
 use App\Repository\CommentRepository;
 use App\Repository\TastingSheetRepository;
+use App\Repository\WorkshopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,10 +20,10 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TastingSheetController extends AbstractController
 {
-    #[Route('/tastingSheet', name: 'app_tasting_sheet')]
-    public function index(Request $request, TastingSheetRepository $tastingSheetRepository, CommentRepository $commentRepository): Response
+    #[Route('/tastingSheet/{codeWorkshop}', name: 'app_tasting_sheet', methods: ['GET', 'POST'])]
+    public function index(Workshop $workshop, Request $request, WorkshopRepository $workshopRepository, TastingSheetRepository $tastingSheetRepository): Response
     {
-       // $comment = new Comment();
+     // $comment = new Comment();
      //   $formComment = $this->createForm(CommentType::class, $comment);
      //   $formComment->handleRequest($request);
 
@@ -32,8 +34,9 @@ class TastingSheetController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
          //   $tastingSheet->setComment($comment);
             //$tastingSheet->setUser($this->getUser());
-          $selectedSmells = $form->get('smell')->getData();
+            $selectedSmells = $form->get('smell')->getData();
             $selectedTastes = $form->get('taste')->getData();
+
             foreach ($selectedSmells as $smell) {
                 $tastingSheet->addSmell($smell);
                 $smell->addTastingSheet($tastingSheet);
@@ -42,6 +45,7 @@ class TastingSheetController extends AbstractController
                 $tastingSheet->addTaste($taste);
                 $taste->addTastingSheet($tastingSheet);
             }
+            $tastingSheet->setWorkshop($workshop);
 
 
            // $commentRepository->save($comment, true);
