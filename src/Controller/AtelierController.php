d99449f4fb019d9cc959c45dd710b676772b2dad
Gibon_david <davidgbn33@gmail.com>
2023-06-26 09:32:37 +0200

    fix gout in profil

--- src/Controller/AtelierController.php
+++ src/Controller/AtelierController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Workshop;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
