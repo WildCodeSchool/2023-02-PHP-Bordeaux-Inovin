c031603a4470e959f09b237ef9b18938717ecc2a
Aurelie Lozach <lozachaurelie@gmail.com>
2023-07-05 15:10:07 +0200

    Flash message modified

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,23 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+    private $emailVerifier;
+
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function index(
+        AuthenticationUtils $authenticationUtils,
+    ): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_home');
         }
 
         $error = $authenticationUtils->getLastAuthenticationError();
-
         $lastUsername = $authenticationUtils->getLastUsername();
+
+
+        if ($error !== null) {
+            if ($error instanceof BadCredentialsException) {
+                $this->addFlash('danger', 'Identifiant(s) invalide(s)');
+            }
+        }
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
